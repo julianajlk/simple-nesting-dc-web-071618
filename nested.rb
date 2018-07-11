038c9e318f18104b059ba84c7ae012e1edd82629
Juliana Kang <julianajlk@gmail.com>
2018-07-11 04:38:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,6 @@ def adding_matz
 
 	programmer_hash =
  		{
-				:yukihiro_matsumoto => {
-			 		:known_for => "Ruby",
-			 		:languages => ["LISP", "C"]
-						}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -86,7 +82,9 @@ def adding_matz
         }
      }
 
-
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
+:languages => ["LISP", "C"]}
+# }
 end
 
 def changing_alan
